807b851661e34e857dbd3fc2beb9b6e0cfc7ad25
shristy <shristychaudhary4679@gmail.com>
2024-12-07 18:11:26 +0530

    added functionality to form

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const addBookBtn = document.getElementById("addBookBtn");
 const submit = document.getElementById("submitBtn");
 const addBookModal = document.getElementById("addBookModal");
+const form = document.getElementById('addBookForm');
 
 const library = [];
 
-function book(name, author, pages, read) {
+function book(name, author, pages, isRead) {
     this.name = name;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.isRead = isRead;
 }
 
-function addToLibrary(name, author, pages, read){
-    const newBook = new book(name, author, pages, read);
+function addToLibrary(name, author, pages, isRead){
+    const newBook = new book(name, author, pages, isRead);
     library.push(newBook);
 }
 
@@ -22,4 +23,17 @@ addBookBtn.addEventListener('click', () => {
 
 submit.addEventListener('click', () => {
     addBookModal.classList.remove("active");
+});
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    const title = form.elements['title'].value;
+    const author = form.elements['author'].value;
+    const pages = form.elements['pages'].value;
+    const isRead = form.elements['isRead'].checked;
+    
+    addToLibrary(title, author, pages, isRead);
+    
+    form.reset();
 });
