876b0e5ea6b0f48fcaa97ed985cda6cf4f0424d3
shristy <shristychaudhary4679@gmail.com>
2024-12-08 11:30:26 +0530

    set up book cards

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ const form = document.getElementById('addBookForm');
 
 const library = [];
 
-function book(name, author, pages, isRead) {
-    this.name = name;
+function Book(title, author, pages, isRead) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
 }
 
-function addToLibrary(name, author, pages, isRead){
-    const newBook = new book(name, author, pages, isRead);
+function addToLibrary(title, author, pages, isRead){
+    const newBook = new Book(title, author, pages, isRead);
     library.push(newBook);
+    createBookCard(newBook);
 }
 
 addBookBtn.addEventListener('click', () => {
@@ -37,3 +38,38 @@ submit.addEventListener('click', (event) => {
     
     form.reset();
 });
+
+function createBookCard(book){
+    const bookCard = document.createElement('div');
+    const bookInfo = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const buttonGroup = document.createElement('div');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+
+    bookCard.classList.add("book-card");
+    bookInfo.classList.add('book-info');
+    bookInfo.append(title, author, pages);
+    buttonGroup.classList.add('btn-group');
+    buttonGroup.append(readBtn, removeBtn);
+    bookCard.append(bookInfo, buttonGroup);
+    title.textContent = `${book.title}`
+    author.textContent = `by ${book.author}`
+    pages.textContent = `${book.pages} pages`
+    removeBtn.textContent = 'Remove'
+    removeBtn.classList.add("remove-btn");
+
+    if (book.isRead) {
+        readBtn.textContent = "Read";
+        readBtn.classList.add('btn-green');
+    }
+    else {
+        readBtn.textContent = "Not read";
+        readBtn.classList.add('btn-red');
+    }
+
+    const bookGrid = document.getElementById('bookGrid');
+    bookGrid.appendChild(bookCard);
+}
